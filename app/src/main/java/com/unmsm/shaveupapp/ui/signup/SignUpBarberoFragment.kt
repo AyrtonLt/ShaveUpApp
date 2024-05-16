5ac8c7e2d5a7eacf55bd38fde5b95a67ab2ac5a7
ayrtonlopezt <ayrton.lopez@unmsm.edu.pe>
2024-05-15 20:41:45 -0500

    Update Project SignUp Cliente and Barbero

--- app/src/main/java/com/unmsm/shaveupapp/ui/signup/SignUpBarberoFragment.kt
+++ app/src/main/java/com/unmsm/shaveupapp/ui/signup/SignUpBarberoFragment.kt
@@ -1,6 +1,8 @@
 package com.unmsm.shaveupapp.ui.signup
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Patterns
 import androidx.fragment.app.Fragment
@@ -8,12 +10,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.unmsm.shaveupapp.R
 import com.unmsm.shaveupapp.databinding.FragmentSignUpBarberoBinding
 import com.unmsm.shaveupapp.ui.menu.barbero.MenuBarberoActivity
+import com.unmsm.shaveupapp.ui.menu.cliente.MenuClienteActivity
 
 class SignUpBarberoFragment : Fragment() {
 
@@ -24,6 +31,11 @@ class SignUpBarberoFragment : Fragment() {
     private var _binding: FragmentSignUpBarberoBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var storageReference: StorageReference
+    private var filePath: Uri? = null
+    private lateinit var storage: FirebaseStorage
+    private lateinit var pickImageLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,169 +44,264 @@ class SignUpBarberoFragment : Fragment() {
 
         auth = FirebaseAuth.getInstance()
 
-        emailFocusListener()
-        passwordFocusListener()
-        firstNameFocusListener()
-        lastNameFocusListener()
-        telephoneFocusListener()
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage.reference
+
+        pickImageLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK && result.data != null && result.data!!.data != null) {
+                    filePath = result.data!!.data
+                }
+            }
+
+        binding.btnChooseImage.setOnClickListener {
+            chooseImage()
+        }
 
         binding.btnCreateUser.setOnClickListener {
-            val validEmail = binding.tilEmail.error == null
-            val validPassword = binding.tilPassword.error == null
-            val validFirstName = binding.tilFirstName.error == null
-            val validLastName = binding.tilLastName.error == null
-            val validTelephone = binding.tilTelefono.error == null
 
-            if (validEmail && validPassword && validFirstName && validLastName && validTelephone) {
+            if (validateInputs()) {
 
-                val email = binding.tietEmail.text.toString()
-                val password = binding.tietPassword.text.toString()
-                val nombre = binding.tietFirstName.text.toString()
-                val apellido = binding.tietLastName.text.toString()
-                val apodo = binding.tietNickName.text.toString()
-                val telefono = binding.tietTelefono.text.toString()
-                val nameBarberia = binding.tietNameBarberia.text.toString()
-                val location = binding.tietDireccion.text.toString()
-                val district = binding.actvDistrito.text.toString()
+                val emailInput = binding.tietEmail.text.toString()
+                val passwordInput = binding.tietPassword.text.toString()
+                val nombreInput = binding.tietFirstName.text.toString()
+                val apellidoInput = binding.tietLastName.text.toString()
+                val apodoInput = binding.tietNickName.text.toString()
+                val telefonoInput = binding.tietTelefono.text.toString()
+                val nameBarberiaInput = binding.tietNameBarberia.text.toString()
+                val locationInput = binding.tietDireccion.text.toString()
+                val districtInput = binding.actvDistrito.text.toString()
 
-
-                if (email.isEmpty() || password.isEmpty() || nombre.isEmpty() || apellido.isEmpty() || telefono.isEmpty()) {
-                    Toast.makeText(
-                        requireContext(),
-                        "Existen campos vacíos",
-                        Toast.LENGTH_LONG
-                    ).show()
-                } else {
-                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val userId = auth.currentUser!!.uid
-                            val usuario = mutableMapOf<String, Any>()
-
-                            usuario["userType"] = "1"
-                            usuario["user_id"] = userId.toString()
-                            usuario["email"] = email
-                            usuario["password"] = password
-                            usuario["nombre"] = nombre
-                            usuario["apellido"] = apellido
-                            usuario["apodo"] = apodo
-                            usuario["telefono"] = telefono
-                            usuario["barberiaNombre"] = nameBarberia
-                            usuario["direccion"] = location
-                            usuario["distrito"] = district
-
-
-                            FirebaseFirestore.getInstance().collection("usuario").document(userId)
-                                .set(usuario)
-                                .addOnSuccessListener {
-                                    Toast.makeText(
-                                        requireContext(),
-                                        "Usuario creado",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                    val intent =
-                                        Intent(requireContext(), MenuBarberoActivity::class.java)
-                                    intent.flags =
-                                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                    startActivity(intent)
-                                }.addOnFailureListener {
-                                    Toast.makeText(
-                                        requireContext(),
-                                        "Ocurrió un error :(",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-
-                                }
+                auth.createUserWithEmailAndPassword(emailInput, passwordInput)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val userId = auth.currentUser?.uid
+                            if (userId != null) {
+                                uploadProfilePhotoandSaveUser(
+                                    userId,
+                                    nombreInput,
+                                    apellidoInput,
+                                    apodoInput,
+                                    telefonoInput,
+                                    nameBarberiaInput,
+                                    locationInput,
+                                    districtInput
+                                )
+                            } else {
+                                showError("Error al obtener el ID del usuario.")
+                            }
+                        } else {
+                            showError("Error al crear el usuario: ${task.exception?.message}")
                         }
                     }
-                }
 
-
+            } else {
+                Toast.makeText(requireContext(), "existen errores", Toast.LENGTH_SHORT).show()
             }
         }
         return binding.root
     }
 
-    private fun emailFocusListener() {
-        binding.tietEmail.setOnFocusChangeListener { _, focused ->
-            if (!focused) {
-                binding.tilEmail.error = validEmail()
-            }
-        }
+    private fun chooseImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        val chooser = Intent.createChooser(intent, "Select Picture")
+        pickImageLauncher.launch(chooser)
     }
 
-    private fun validEmail(): String? {
-        val emailText = binding.tietEmail.text.toString()
-        if (!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()) {
-            return "Email inválido"
-        }
-        return null
-    }
+    private fun validateInputs(): Boolean {
+        var isValid = true
 
-    private fun passwordFocusListener() {
-        binding.tietPassword.setOnFocusChangeListener { _, focused ->
-            if (!focused) {
-                binding.tilPassword.error = validPassword()
-            }
+        // Validación Email
+        val emailInput = binding.tietEmail.text.toString().trim()
+        if (emailInput.isEmpty()) {
+            binding.tilEmail.error = "Esta campo es obligatorio"
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            binding.tilEmail.error = "Email inválido"
+        } else {
+            binding.tilEmail.error = null
         }
-    }
 
-    private fun validPassword(): String? {
-        val passwordText = binding.tietPassword.text.toString()
-        if (passwordText.length < 6) {
-            return "Mínimo 6 dígitos"
+        // Validación Password
+        val passwordInput = binding.tietPassword.text.toString().trim()
+        if (passwordInput.isEmpty()) {
+            binding.tilPassword.error = "La contraseña no puede estar vacía"
+            isValid = false
+        } else if (passwordInput.length < 6) {
+            binding.tilPassword.error = "La contraseña debe tener al menos 6 caracteres"
+            isValid = false
+        } else {
+            binding.tilPassword.error = null
         }
-        return null
-    }
 
-    private fun firstNameFocusListener() {
-        binding.tietFirstName.setOnFocusChangeListener { _, focused ->
-            if (!focused) {
-                binding.tilFirstName.error = validFirstName()
-            }
-        }
-    }
-
-    private fun validFirstName(): String? {
-        val firstNameText = binding.tietFirstName.text.toString()
+        //Validación Nombre
+        val firstNameInput = binding.tietFirstName.text.toString().trim()
         val regex = Regex("^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ ]+$")
-
-        if (!firstNameText.matches(regex)) {
-            return "El nombre no es válido"
+        if (firstNameInput.isEmpty()) {
+            binding.tilFirstName.error = "Este campo es obligatorio"
+            isValid = false
+        } else if (!firstNameInput.matches(regex)) {
+            binding.tilFirstName.error = "Hay caracteres no permitidos"
+            isValid = false
+        } else {
+            binding.tilFirstName.error = null
         }
-        return null
+
+        // Validación Apellido
+        val lastNameInput = binding.tietLastName.text.toString().trim()
+        if (lastNameInput.isEmpty()) {
+            binding.tilLastName.error = "El apellido no puede estar vacío"
+            isValid = false
+        } else if (!lastNameInput.matches(regex)) {
+            binding.tilLastName.error = "Hay caracteres no permitidos"
+            isValid = false
+        } else {
+            binding.tilLastName.error = null
+        }
+
+        //Validación Apodo
+        val nickNameInput = binding.tietNickName.text.toString().trim()
+        val regexNickName = "^[a-zA-Z0-9_-]*$".toRegex()
+        if (!regexNickName.matches(nickNameInput)) {
+            binding.tilNickName.error = "Hay caracteres no permitidos"
+            isValid = false
+        } else {
+            binding.tilNickName.error = null
+        }
+
+        // Validacion Telefono
+        val phoneInput = binding.tietTelefono.text.toString().trim()
+        if (phoneInput.isEmpty()) {
+            binding.tietTelefono.error = "Este campo es obligatorio"
+            isValid = false
+        } else {
+            binding.tietTelefono.error = null
+        }
+
+        //Validación Barberia
+        val barberiaNameInput = binding.tietNameBarberia.text.toString().trim()
+        val regexBarberia = "^[a-zA-Z0-9_-]*$".toRegex()
+        if (!regexBarberia.matches(barberiaNameInput)) {
+            binding.tilNameBarberia.error = "Hay caracteres no permitidos"
+            isValid = false
+        } else if (barberiaNameInput.isEmpty()) {
+            binding.tilNameBarberia.error = "Este campo es obligatorio"
+            isValid = false
+        } else {
+            binding.tilNameBarberia.error = null
+        }
+
+        //Validación Direccion
+        val locationInput = binding.tietDireccion.text.toString().trim()
+        val regexLocation = "^[a-zA-Z0-9_-]*$".toRegex()
+        if (!regexLocation.matches(locationInput)) {
+            binding.tilDireccion.error = "Hay caracteres no permitidos"
+            isValid = false
+        } else if (locationInput.isEmpty()) {
+            binding.tilDireccion.error = "Este campo es obligatorio"
+            isValid = false
+        } else {
+            binding.tilDireccion.error = null
+        }
+
+        //Validación Distrito
+        val districtInput = binding.actvDistrito.text.toString().trim()
+        if (districtInput.isEmpty()) {
+            binding.tilDistrito.error = "Tiene que seleccionar una opción"
+            isValid = false
+        } else {
+            binding.tilDistrito.error = null
+        }
+
+        return isValid
     }
 
-    private fun lastNameFocusListener() {
-        binding.tietLastName.setOnFocusChangeListener { _, focused ->
-            if (!focused) {
-                binding.tilLastName.error = validLastName()
+    private fun uploadProfilePhotoandSaveUser(
+        userId: String,
+        firstName: String,
+        lastName: String,
+        apodo: String,
+        telefono: String,
+        nameBarberia: String,
+        location: String,
+        district: String
+    ) {
+        if (filePath != null) {
+            val ref = storageReference.child("profilePhoto/$userId")
+            ref.putFile(filePath!!)
+                .addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener { uri ->
+                        val profilePhotoUrl = uri.toString()
+                        saveUserToFirestore(
+                            userId, firstName, lastName, apodo,
+                            telefono,
+                            nameBarberia,
+                            location,
+                            district, profilePhotoUrl
+                        )
+                    }
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(requireContext(), "Failed " + e.message, Toast.LENGTH_SHORT)
+                        .show()
+                }
+        } else {
+            saveUserToFirestore(
+                userId, firstName, lastName, apodo,
+                telefono,
+                nameBarberia,
+                location,
+                district, ""
+            )
+        }
+    }
+
+    private fun saveUserToFirestore(
+        userId: String,
+        firstName: String,
+        lastName: String,
+        apodo: String,
+        telefono: String,
+        nameBarberia: String,
+        location: String,
+        district: String,
+        imageUrl: String
+    ) {
+        val usuario = mutableMapOf<String, Any>()
+        usuario["userType"] = "1"
+        usuario["user_id"] = userId
+        usuario["nombre"] = firstName
+        usuario["apellido"] = lastName
+        usuario["apodo"] = apodo
+        usuario["telefono"] = telefono
+        usuario["barberiaNombre"] = nameBarberia
+        usuario["direccion"] = location
+        usuario["distrito"] = district
+
+        //Sólo si existe foto de perfil seleccionada
+        if (!imageUrl.isEmpty()) {
+            usuario["urlProfilePhoto"] = imageUrl
+        }
+
+        FirebaseFirestore.getInstance().collection("usuario").document(userId)
+            .set(usuario)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "Usuario creado", Toast.LENGTH_SHORT).show()
+                navigateToMenuBarbero()
             }
-        }
-    }
-
-    private fun validLastName(): String? {
-        val lastNameText = binding.tietLastName.text.toString()
-        val regex = Regex("^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ ]+$")
-
-        if (!lastNameText.matches(regex)) {
-            return "El nombre no es válido"
-        }
-        return null
-    }
-
-    private fun telephoneFocusListener() {
-        binding.tietTelefono.setOnFocusChangeListener { _, focused ->
-            if (!focused) {
-                binding.tilTelefono.error = validPhone()
+            .addOnFailureListener { e ->
+                showError("Ocurrió un error al guardar el usuario: ${e.message}")
             }
-        }
     }
 
-    private fun validPhone(): String? {
-        val phoneText = binding.tietTelefono.text.toString()
-        if (phoneText.length != 9) {
-            return "Número inválido"
-        }
-        return null
+    private fun navigateToMenuBarbero() {
+        val intent = Intent(requireContext(), MenuBarberoActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
